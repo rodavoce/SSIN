eafc1894e08ade9fceae7b54f6154eb8715c5a2b
BruunoMarques <pandasarehere@gmail.com>
2018-05-16 03:02:58 +0100

    added respective lines for connected to DB

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,30 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 const store = createStore(rootReducer);
 
+const driver = require('bigchaindb-driver')
+
+
+const alice = new driver.Ed25519Keypair()
+const conn = new driver.Connection(
+    'https://test.bigchaindb.com/api/v1/',
+    { app_id: 'af2e016d',
+      app_key: '405c9c673ddcf8a68807dbc4c45ef5e1' })
+
+const tx = driver.Transaction.makeCreateTransaction(
+        { message: 'testmessage' },
+        null,
+        [ driver.Transaction.makeOutput(
+            driver.Transaction.makeEd25519Condition(alice.publicKey))],
+        alice.publicKey)
+    const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
+    conn.postTransactionCommit(txSigned)
+
+
+conn.searchAssets('testmessage')
+.then(assets => console.log('Found assets with serial number Bicycle Inc.:', assets))  
+
+
+
 render(
   <Provider store={store} >
     <App />
