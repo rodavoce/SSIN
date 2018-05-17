1d607464ad4e258de313f314a3a8b162fbbe1f77
BruunoMarques <pandasarehere@gmail.com>
2018-05-17 06:12:35 +0100

    added key hashing

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ const app = express();
 const port = 3001;
 const driver = require('bigchaindb-driver');
 
+
+const PromiseA = require('bluebird').Promise;
+const fs = PromiseA.promisifyAll(require('fs'));
+const path = require('path');
+const ursa = require('ursa');
+const mkdirpAsync = PromiseA.promisify(require('mkdirp'));
+const pathname = 'serverkeys'
+
+
 const alice = new driver.Ed25519Keypair();
 const conn = new driver.Connection(
     'https://test.bigchaindb.com/api/v1/',
@@ -36,7 +45,7 @@ app
 app.listen(port, (err) =>  {
     if(err) {
         return console.log('maybe the port isnt avaiable');
-    }
+   }
 
     console.log("server is listening on port " + port)
 });
@@ -85,14 +94,51 @@ const timestamp = function (hash) {
 
 
 const verifyTimestamp  = function (signature) {
+    var privkeyServer = ursa.createPrivateKey(fs.readFileSync('./'+ pathname +'/privkey.pem'));
+    var pubkeyServer = ursa.createPublicKey(fs.readFileSync('./'+ pathname +'/pubkey.pem'));
+
+    var msg = "IT’S A SECRET TO EVERYBODY.";
+    var sig;
+    var enc;
+    enc = pubkeyServer.encrypt(msg, 'utf8', 'base64');
+    sig = privkeyServer.hashAndSign('sha256', msg, 'utf8', 'base64'); //é este que vamos querer fazer aqui
+    console.log('encrypted', enc, '\n');
+    console.log('signed', sig, '\n');
+
     //apply public key over signature
     //should match the received one
 };
 
 
+if (fs.existsSync(pathname)){
+    console.log('keys were previously created');
+} 
+else  {
+PromiseA.all([
+    setkey('serverkeys')    
+  ]).then(function (keys) {
+    console.log('generated %d keypairs', keys.length);
+  }); 
+}  
+ 
 
 
+function setkey(pathname) {
+    const key = ursa.generatePrivateKey(1024, 65537);
+    const privpem = key.toPrivatePem();
+    const pubpem = key.toPublicPem();
+    const privkey = path.join(pathname, 'privkey.pem');
+    const pubkey = path.join(pathname, 'pubkey.pem');;
+
+        return mkdirpAsync(pathname).then(function () {
+            return PromiseA.all([
+              fs.writeFileAsync(privkey, privpem, 'ascii')
+            , fs.writeFileAsync(pubkey, pubpem, 'ascii')
+            ]);
+          }).then(function () {
+            return key;
+          });
+  };
 
 
-
-
+verifyTimestamp("asdsad");
